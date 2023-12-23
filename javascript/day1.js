a7e33a567b240431fd41658404626f71870abfba
daniel-forrester <danielf216@gmail.com>
2023-12-22 21:57:14 -0500

    day1 done

--- javascript/day1.js
+++ javascript/day1.js
@@ -1,10 +1,9 @@
 import { day1Data, day1Test } from "../db/day1.mjs";
 
-const result = day1Test.reduce((acc, currentEl) => {
+const result = day1Data.reduce((acc, currentEl) => {
   let number = currentEl.split("").filter((char) => !isNaN(char));
   let x = number.length >= 1 ? number[0] : "";
-  let y =
-    number.length > 1 ? Number(number[number.length - 1]) : Number(number[0]);
+  let y = number.length > 0 ? Number(number[number.length - 1]) : "";
   let total = Number(x + y);
   console.log(`Total: ${total}`);
   return (acc = acc + total);
